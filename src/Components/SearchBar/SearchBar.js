6085fcdd6b600ae889f09a9b58574dba1273986e
Patrick O'Connor <pattyo20@optonline.net>
2022-09-26 17:38:52 -0400

    Finished spotify file to link to API

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,11 +3,30 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+
+        this.seacrh = this.seacrh.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    seacrh() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event) {
+        this.setState({term: event.target.value});
+    }
+
     render() {
         return(
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton"onClick={this.seacrh}>SEARCH</button>
             </div>
         )
     }
